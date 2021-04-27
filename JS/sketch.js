9b49a7bf6613379ac87e72b8dd3ce24fee0fbd31
Gerardo Vargas-Hernandez <gvarga3@students.towson.edu>
2021-04-26 21:35:58 -0400

    Adding Ball to Game
    Creating a Ball

--- JS/sketch.js
+++ JS/sketch.js
@@ -1,7 +1,30 @@
+var paddle;
+var ball;
+
 function setup() {
     createCanvas(500, 500);
+    paddle = new Paddle();
+    ball = new Ball();
 }
 
 function draw() {
     background(0);
+    paddle.display();
+    paddle.update();
+    paddle.checkEdges();
+
+    ball.display();
+}
+
+function keyPressed() {
+    if (keyCode === LEFT_ARROW) {
+        paddle.isMovingLeft = true;
+    } else if (keyCode === RIGHT_ARROW) {
+        paddle.isMovingRight = true;
+    }
+}
+
+function keyReleased() {
+    paddle.isMovingLeft = false;
+    paddle.isMovingRight = false;
 }
