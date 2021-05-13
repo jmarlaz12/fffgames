a5628ddff69e346a5ca14f1aea65c13833fefc31
Gerardo Vargas-Hernandez <gvarga3@students.towson.edu>
2021-05-12 22:53:23 -0400

    Finishing Touches - Skeleton
    The site body layout and design is complete

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,12 @@
-let test;
+const navBar = document.querySelector('.normal');
+const navHeight = navBar.getBoundingClientRect().height;
+console.log(navHeight);
+
+window.addEventListener("scroll", () => {
+    const scrollHeight = window.pageYOffset;
+    if (scrollHeight > navHeight) {
+        navBar.classList.add("fixed");
+    } else {
+        navBar.classList.remove("fixed");
+    }
+});
