fb09ae746fb15668bc42e29ee1b07f4bea435a9f
Gerardo Vargas-Hernandez <gvarga3@students.towson.edu>
2021-05-12 23:22:21 -0400

    Final Animations - Site
    Everything is complete on the main site

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 const navBar = document.querySelector('.normal');
 const navHeight = navBar.getBoundingClientRect().height;
-console.log(navHeight);
+var all = document.querySelectorAll('.game');
 
 window.addEventListener("scroll", () => {
     const scrollHeight = window.pageYOffset;
@@ -10,3 +10,10 @@ window.addEventListener("scroll", () => {
         navBar.classList.remove("fixed");
     }
 });
+
+// Animations
+for (var i = 0; i < all.length; i++) {
+    gsap.from(all[i], { opacity: 0, duration: .1, delay: 0.5 + (i * 0.1), x: -200 });
+}
+
+gsap.from(".normal", { opacity: 0, duration: 2, delay: 0.5, y: -10 });
